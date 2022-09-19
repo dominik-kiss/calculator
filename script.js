4aee29ff350a0516f3d70c793545128784e91dc2
dominik-kiss <dominik.kiss997@gmail.com>
2022-09-19 09:23:44 +0200

    Create basic operator functions

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+// Functions for the basic math operators
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+// Function to decide which basic operation should be called
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case +:
+            add(num1, num2);
+            break;
+        case -:
+            subtract(num1, num2);
+            break;
+        case *:
+            multiply(num1, num2);
+            break;
+        case /:
+            divide(num1, num2);
+            break;                  
+    }
+}
