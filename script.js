e73fcb1690ae8a3be8b61d43a3004250a123e083
dominik-kiss <dominik.kiss997@gmail.com>
2022-09-19 13:54:18 +0200

    Add selectors, event listeners, basic input functions

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 // Functions for the basic math operators
 
 function add(a, b) {
@@ -35,4 +33,29 @@ function operate(operator, num1, num2) {
             divide(num1, num2);
             break;                  
     }
+}
+
+// Listen for number button clicks and update display accordingly
+
+let numButtons = document.querySelectorAll(".number");
+
+numButtons.forEach(button => button.addEventListener("click", inputNum));
+
+let display = document.getElementById("display");
+
+function inputNum() {
+    display.textContent += this.textContent;
+}
+
+// Listen for operator clicks and store number and operator 
+
+let operators = document.querySelectorAll(".operator");
+
+operators.forEach(operator => operator.addEventListener("click", storeNum));
+
+let num1 = 0;
+
+function storeNum() {
+    num1 = display.textContent;
+    display.textContent = this.textContent;
 }
